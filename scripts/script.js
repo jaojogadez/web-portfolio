835d7ad8fd644da11409aa1dc3d62cd7e5850eef
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-06 07:51:06 -0300

    começei a criar a função createProject

--- scripts/script.js
+++ scripts/script.js
@@ -16,13 +16,36 @@ function toggleMode(){
     }
 }
 
-// window.addEventListener("scroll", function(){
-//     let scrollTop = this.window.scrollY
-//     let nav = this.document.querySelector(".navbar")
-//     console.log(scrollTop)
-//     if(scrollTop > 700){
-//         nav.classList.add("fixed") 
-//     }else{
-//         nav.classList.remove("fixed")
-//     }
-// })
+let links = {
+    access: "https://example.com",
+    code: "https://example.com"
+}
+
+function createNewProject(name, image, description, links, skills){
+    const strutureProject = `
+        <div class="col" data-aos="fade-up">
+          <div class="shadow-lg rounded-bottom rounded-top">
+            <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-1 shadow-sm rounded-top" style="background-image: url('${image}')"></div>
+            <div class="card-body text-bg-light p-3 rounded-bottom">
+              <h3 class="lh-1 fw-semibold" id="name">${name}</h3>
+              <p class="card-text" id="description">
+                ${description}
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="${links.access}">
+                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="bi bi-app-indicator"></i> Access</button>
+                  </a>
+                  <a href="${links.code}">
+                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="bi bi-github"></i> Code</button>
+                  </a>
+                </div>
+                <small class="text-muted" id="skills">      
+                  
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+    `
+}
