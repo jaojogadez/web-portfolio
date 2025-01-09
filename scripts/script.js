16da4f99c4e0b95bf8cc9f3c2e0b87ae587e26ea
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-09 11:35:03 -0300

    deu certo

--- scripts/script.js
+++ scripts/script.js
@@ -66,3 +66,43 @@ function createNewProject(name, image, description, links, skills){
   newProjectPlace.insertAdjacentHTML("beforebegin", strutureProject)
 }
 
+// function add img start
+const label = document.querySelector(".file-input")
+
+function onEnter(){
+  label.classList.add("active")
+}
+
+function onLeave(){
+  label.classList.remove("active")
+}
+
+label.addEventListener("dragenter", onEnter)
+label.addEventListener("drop", onLeave)
+label.addEventListener("dragend", onLeave)
+label.addEventListener("dragleave", onLeave)
+
+const input = document.querySelector(".file-input input")
+const dropzone = document.querySelector("#drop-zone")
+
+input.addEventListener("change", () => {
+  if(input.files.length > 0){
+    const type = input.files[0].type
+    const formats = ["image/jpeg", "image/png", "image/jpg"]
+    if(!formats.includes(type)){
+      alert("Formato não permitido")
+      return
+    }
+    
+    if(document.querySelector("#cover")){
+      dropzone.removeChild(document.querySelector("#cover"))
+    }
+
+    const img = document.createElement("img")
+    img.id = "cover"
+    img.src = URL.createObjectURL(input.files[0])
+    
+    dropzone.appendChild(img)  
+  }  
+})
+// function add img end
