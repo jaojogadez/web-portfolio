2989d158979fc28bf42b0d4b4f5719a8b44aff13
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-09 18:21:04 -0300

    recomeçei a função criar projeto

--- scripts/script.js
+++ scripts/script.js
@@ -26,55 +26,38 @@ function toggleMode(){
   }
 }
 
+//const addBtn = document.querySelector(".bi-plus-square")
+//const newProjectPlace = addBtn.parentElement.parentElement
 
-const form = document.querySelector("form")
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  
-
-  createNewProject(name, image, description, links, skills)
-})
-
-
-let skills = []
-
-let links = {
-  access: "https://example.com",
-  code: "https://example.com"
-}
-
-const addBtn = document.querySelector(".bi-plus-square")
-const newProjectPlace = addBtn.parentElement.parentElement
-
-function createNewProject(name, image, description, links, skills){
-  const strutureProject = `
-        <div class="col" data-aos="fade-up">
-          <div class="shadow-lg rounded-bottom rounded-top">
-            <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-1 shadow-sm rounded-top" style="background-image: url('${image}')"></div>
-            <div class="card-body text-bg-light p-3 rounded-bottom">
-              <h3 class="lh-1 fw-semibold" id="name">${name}</h3>
-              <p class="card-text" id="description">
-                ${description}
-              </p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <a href="${links.access}" target="_blank">
-                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="bi bi-app-indicator"></i> Access</button>
-                  </a>
-                  <a href="${links.code}" target="_blank">
-                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="bi bi-github"></i> Code</button>
-                  </a>
-                </div>
-                <small class="text-muted" id="skills">
-                ${skills.map(skill => ` <i class="${skill}"></i>` ).join('')}
-                </small>
-              </div>
-            </div>
-          </div>
-        </div>
-    `
-  newProjectPlace.insertAdjacentHTML("beforebegin", strutureProject)
-}
+// function createNewProject(name, image, description, links, skills){
+//   const strutureProject = `
+//         <div class="col" data-aos="fade-up">
+//           <div class="shadow-lg rounded-bottom rounded-top">
+//             <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-1 shadow-sm rounded-top" style="background-image: url('${image}')"></div>
+//             <div class="card-body text-bg-light p-3 rounded-bottom">
+//               <h3 class="lh-1 fw-semibold" id="name">${name}</h3>
+//               <p class="card-text" id="description">
+//                 ${description}
+//               </p>
+//               <div class="d-flex justify-content-between align-items-center">
+//                 <div class="btn-group">
+//                   <a href="${links.access}" target="_blank">
+//                     <button type="button" class="btn btn-sm btn-outline-secondary"><i class="bi bi-app-indicator"></i> Access</button>
+//                   </a>
+//                   <a href="${links.code}" target="_blank">
+//                     <button type="button" class="btn btn-sm btn-outline-secondary"><i class="bi bi-github"></i> Code</button>
+//                   </a>
+//                 </div>
+//                 <small class="text-muted" id="skills">
+//                 ${skills.map(skill => ` <i class="${skill}"></i>` ).join('')}
+//                 </small>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//     `
+//   newProjectPlace.insertAdjacentHTML("beforebegin", strutureProject)
+// }
 
 /* CREATE IMG MODAL START */
 const label = document.querySelector(".file-input")
@@ -123,3 +106,23 @@ function createIMG(url){
 }
 
 /* CREATE IMG MODAL END */
+
+
+/* CREATE PROJECT START */
+const form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let skills = document.querySelectorAll('input[type="checkbox"]:checked')
+  let technologies = Array.from(skills).map(checkbox => checkbox.value)
+  let project = {
+    name: document.querySelector("#floatingName").value,
+    imgurl: URL.createObjectURL(input.files[0]),
+    description: document.querySelector("#floatingDescription").value,
+    links: {
+      access: document.querySelector("#floatingLinkAccess").value,
+      code: document.querySelector("#floatingLinkCode").value,
+    },
+    technologies
+  }
+})
+/* CREATE PROJECT END */
